0ca99c6d820364aa08079ee3d211d22dc0cf2727
Hassaan-Mahmood <hassaan2980@gmail.com>
2026-07-26 23:37:02 +0500

    Add functionality for 'AC' and 'C' buttons in calculator

--- app.js
+++ app.js
@@ -13,9 +13,21 @@ function calculator(parameter) {
   ) {
     operator = parameter;
     currentDisplay.innerHTML += operator;
+  } else if (parameter === "AC") {
+    currentDisplay.innerHTML = "";
+  }
+  //  else if (parameter === "C") {
+  //   let string = currentDisplay.innerHTML;
+  //   string.substring(0, string-1);
+  // }
+  else {
+    if (operator === "") {
+      firstNumber = parameter;
+    } else {
+      secondNumber = parameter;
+    }
+    currentDisplay.innerHTML += parameter;
   }
 }
 
-
 function equalOperation() {}
-  
